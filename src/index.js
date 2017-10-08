7ea35df7a947637d4fb52cb9f9fa12c53f8791a5
unknown <damien.nunes88@gmail.com>
2017-10-08 17:27:25 +0200

    testing webpack bundles

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './index.css';
+import numeral from 'numeral';
+
+const someValue = numeral(1000).format('$0,0.00');
+console.log(`Testing webpack bundle.js compilation with "numeral" module formating 1000 to --> ${someValue} <---`);
