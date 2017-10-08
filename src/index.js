a0b20bd16ade254fbad4a4b36a9d2103c9ecee37
unknown <damien.nunes88@gmail.com>
2017-10-08 18:24:37 +0200

    eslint added + automated scripts with eslint

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './index.css';
 import numeral from 'numeral';
 
 const someValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`Testing webpack bundle.js compilation with "numeral" module formating 1000 to --> ${someValue} <---`);
+
+console.log(`Testing webpack bundle.js compilation with "numeral" module formating 1000 to --> ${someValue} <---`); // eslint-disable-line no-console
